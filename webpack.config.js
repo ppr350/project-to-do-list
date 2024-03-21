634eb45f9e13d3cee08b3c6a8315c20f671d0e48
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-03-21 18:08:49 +0000

    Fxing CSS 16

--- webpack.config.js
+++ webpack.config.js
@@ -24,18 +24,18 @@ module.exports = {
     optimization: {
         runtimeChunk: 'single',
     },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.css$/i,
-    //             use: ['style-loader', 'css-loader'],
-    //         },
-    //         {
-    //             test: /\.(png|svg|jpg|jpeg|gif)$/i,
-    //             type: 'asset/resource',
-    //           },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+              },
         
-    //     ],
-    // },
+        ],
+    },
 
 };
